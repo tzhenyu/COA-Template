321625c3c9879836ac9130ecdfbb583cc01bc2aa
Tan Zhen Yu <81618051+tzhenyu@users.noreply.github.com>
2025-08-21 16:22:40 +0800

    Update main.asm

--- BMCS1113-Assignmment/main.asm
+++ BMCS1113-Assignmment/main.asm
@@ -1,6 +1,37 @@
-INCLUDE Irvine32.inc
+; AddTwoSum.asm - Chapter 3 example.
+include irvine32.inc
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
 
 .data
+coma byte ", ",0
+number dword 70019,9003,3001,4,6,1
+temp dword ?
+two word 2
 
 .code
+main proc
+	
+	mov ecx,6
+	mov esi,0
 
+l1: mov ebx,number[esi]
+	mov temp,ebx
+	mov dx,word ptr temp+2
+	mov ax,word ptr temp
+	div two 
+
+	cmp dx,0
+	jne	NEQT
+	call writeInt
+	mov edx,offset coma
+	call writeString
+	NEQT:add esi,4
+		loop l1
+
+
+	invoke ExitProcess,0
+main endp
+end main
